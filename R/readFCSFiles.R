601e1815c88408d9390949d4a0b90ae18d0db368
KoenAStam <koen.a.stam@gmail.com>
2020-03-17 11:05:45 +0100

    Added documentation

--- R/readFCSFiles.R
+++ R/readFCSFiles.R
@@ -1,6 +1,28 @@
+#' Reads .fcs files
+#'
+#' @description
+#' A wrapper function around \code{\link[flowCore]{read.FCS}} to read .fcs
+#' files. It will only keep relevant channels and renames them according to
+#' the `FCSMeta` parameter.
 #'
 #'
-readFCS <- function(path=".", downsampling, FCSMeta, asinh5=TRUE){
+#' @param path A character string indicating the location of the .fcs files. The
+#' default corresponds to the working directory
+#' @param FCSMeta A list holding meta information on the .fcs files. The default
+#' is set to \code{\link[massive]{readFCSMeta}}
+#' @param asinh5 Logical, should the data be transformed with the arc-sinh
+#' cofactor 5
+#' @param downsampling An integer that indicates the size to downsample to.
+#' If not supplied, no downsampling will be performed.
+#'
+#' @keywords read, data, FCS, files
+#'
+#' @examples
+#' FCSDir <- system.file("extdata", package="massive")
+#' FCSData <- readFCSFiles(path=FCSDir)
+#'
+#' @export
+readFCSFiles <- function(path=".", FCSMeta, asinh5=TRUE, downsampling){
     FCSPaths <- list.files(path=path, pattern=".fcs", full.names=TRUE)
     FCSNames <- list.files(path=path, pattern=".fcs", full.names=FALSE)
 
@@ -30,6 +52,8 @@ readFCS <- function(path=".", downsampling, FCSMeta, asinh5=TRUE){
             if(nSample < downsampling){
                 warning("downsampling size exceeds number of events, set to max")
             }
+        } else {
+            nSample = NULL
         }
 
         FCSFile <- flowCore::read.FCS(FCSPaths[[i]],
