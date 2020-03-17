7f3537e8e696a4432b4f401f5207fa750ddd2177
KoenAStam <koen.a.stam@gmail.com>
2020-03-17 15:43:35 +0100

    Fixed parameter typo

--- R/runSOM.R
+++ R/runSOM.R
@@ -1,14 +1,15 @@
 #' Run SOM
 #'
-runSOM <- function(FCSData, xdim=15, ydim=15, iter=10){
+#' @export
+runSOM <- function(x, xdim=15, ydim=15, iter=20){
 
-    x <- as.matrix(fcsData[,setdiff(colnames(x), "sampleID")])
+    x <- as.matrix(x[,setdiff(colnames(x), "sampleID")])
     somGrid <- somgrid(xdim, ydim, topo="hexagonal")
     somModel <- som(x,
                     grid=somGrid,
                     rlen=iter,
                     alpha=c(0.05,0.01),
-                    keep.data=FALSE)
+                    keep.data=TRUE)
 
     return(somModel)
 }
