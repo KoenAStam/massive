2cbc7927ed41e99cffb6761ec23d5585d9859828
KoenAStam <koen.a.stam@gmail.com>
2020-03-30 14:52:38 +0200

    Added data file with markers

---
+++ R/markers-data.R
@@ -0,0 +1,14 @@
+#' Marker antibodies list
+#'
+#' A dataset containing a comprehensive list with marker antibodies, which is
+#' largely based on the Fluidigm catalog for CyTOF.
+#'
+#' @format A data frame with 395 observations and 3 variables:
+#' \describe{
+#'  \item{marker}{}
+#'  \item{label}{}
+#'  \item{class}{}
+#'  }
+#'
+#' @source \url{https://www.fluidigm.com/catalog?}
+"markers"
